cd99491214d31ced8f1202f5a53f3bc67ec91e7e
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-04-22 13:36:28 +0200

    some planning

--- src/platform/handler.rs
+++ src/platform/handler.rs
@@ -1,4 +1,10 @@
-use axum::{Json, Router, extract::State, http::StatusCode, response::IntoResponse, routing::post};
+use axum::{
+    Json, Router,
+    extract::State,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{delete, get, patch, post},
+};
 
 use crate::{
     AppState,
@@ -6,8 +12,20 @@ use crate::{
     platform::{service, types::LoginRequest},
 };
 
+// NOTE:
+// - DELETE /users/:id -> owner can delete all but himself, admin can delete role='user'
+// - POST /users/:id/role -> owner can modify roles
 pub fn router() -> Router<AppState> {
-    Router::new().route("/login", post(login))
+    Router::new()
+        .route("/login", post(login))
+        .route("/logout", post(async || {}))
+        .route("/users", get(async || {}))
+        .route("/users", post(async || {}))
+        .route("/users/:id", get(async || {}))
+        .route("/users/:id", delete(async || {}))
+        .route("/users/:id/role", post(async || {}))
+        .route("/me", get(async || {}))
+        .route("/me", patch(async || {}))
 }
 
 async fn login(
