c6f3d8c1c1fd42c3a42c3788412f41b7e447b01e
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-04-29 20:10:18 +0200

    I don't really feel like writing a commit

--- src/platform/service.rs
+++ src/platform/service.rs
@@ -52,7 +52,7 @@ pub async fn create_user(
 }
 
 // Currently I hash the password while locking the db lock. Not ideal but right now it feels like
-// the better solution, otherwise I'd need to validate EMAIL and PASSWORD before acquiring it
+// the better solution, otherwise I'd need to validate EMAIL and PASSWORD before acquiring it,
 // hurting the flexibility of bootstrap that I want (credentials not needed if DB is already seeded)
 pub async fn ensure_owner(
     pool: &PgPool,
